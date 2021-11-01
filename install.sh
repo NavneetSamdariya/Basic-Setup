05d0355ca3a759fc4ec7a22364389bab3e2fe734
NavneetSamdariya <25612566+NavneetSamdariya@users.noreply.github.com>
2021-11-01 10:39:32 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt-get install gnome-tweaks gnome-shell-extensions dash-to-dock telegram-desktop snapd wget openssh-server x11vnc -y
+sudo snap install discord spotify brave gnome-system-monitor 
+#etc.
